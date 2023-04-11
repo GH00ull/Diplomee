26480a2512e63fbcb88c07bbac3165ef577a16bb
GH00ull <islam200378@gmail.com>
2023-04-11 16:46:05 +0500

    иправление миграции

--- database/migrations/2023_04_09_224124_create_sheets_table.php
+++ database/migrations/2023_04_09_224124_create_sheets_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,9 @@ return new class extends Migration
         Schema::create('sheets', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('opis');
+            $table->unsignedBigInteger('userid');
+            $table->foreign('userid')->references('id')->on('users');
             $table->softDeletes();
         });
     }
